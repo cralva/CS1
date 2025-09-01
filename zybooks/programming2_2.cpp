a51c89af0152911a0a2c844190c990ce31fc4ad0
cralva <caa081297@gmail.com>
2025-09-01 14:51:59 -0400

    touch myFile.txt mins in

--- zybooks/programming2_2.cpp
+++ zybooks/programming2_2.cpp
@@ -3,3 +3,14 @@
     //2. Conditional logic (if -else can also be called a decision)
     //3. Iteration logic (loops)
     //3. nesting and stacking
+
+    //8 bits = 1 byte (a byte is a char or character in cpp)
+
+//we have coding systems: ASCII, unicode, ebcdic (original coding system that ibm created)
+
+//google "ascii wiki" to see what it stands for and see some examples
+
+//when assigning a value for a variable if we do it twice then the first one will get replaced with the newest variable
+    //numChars = 5
+    //numChars = 3
+    //If we want an output then the 5 will get ignored and the three will be the output
