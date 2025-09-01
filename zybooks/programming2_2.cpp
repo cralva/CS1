fed60b071ca2070679c7f2287e8fd2ebf127df74
cralva <caa081297@gmail.com>
2025-09-01 14:13:12 -0400

    12minute mark

---
+++ zybooks/programming2_2.cpp
@@ -0,0 +1,5 @@
+//The three programming constructs
+    //1. Sequential logic: by default. 
+    //2. Conditional logic (if -else can also be called a decision)
+    //3. Iteration logic (loops)
+    //3. nesting and stacking
